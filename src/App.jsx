f54d3e867939b1b648de3fb5ac4240269858ce70
TangDuYen <tangduyen12345@gmail.com>
2025-05-18 22:15:20 +0700

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss'
 
 import { persistor, store } from './redux/store';
 
+import Login from './pages/login/Login';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux";
 import { RouterProvider } from 'react-router-dom';
@@ -10,11 +11,12 @@ import { router } from "./routers/router";
 function App() {
   return (
     <>
-      <Provider store={store}>
+    <Login />
+      {/* <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <RouterProvider router={router} />
         </PersistGate>
-      </Provider>
+      </Provider> */}
     </>
   )
 }
