680c51c2477ddf3d1b9ffbc3a69b615bc78a0c73
TangDuYen <tangduyen12345@gmail.com>
2025-05-20 23:48:02 +0700

    config auth api for registe, login, pincode

--- src/components/GetToken.jsx
+++ src/components/GetToken.jsx
@@ -2,7 +2,6 @@ import { jwtDecode } from "jwt-decode";
 
 export const token = localStorage.getItem('token') && jwtDecode(localStorage.getItem('token'))
 
-
 export const getToken = ()=>{
     let token = localStorage.getItem('token')
     return token ? jwtDecode(token) : null;
