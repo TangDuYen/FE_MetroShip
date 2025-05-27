e3cf1cd2d29f6aa271c845c3c7df2a0debe864df
TangDuYen <tangduyen12345@gmail.com>
2025-05-27 18:56:01 +0700

    add toast message, config authen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
+import 'react-toastify/dist/ReactToastify.css';
 
 import { persistor, store } from './redux/store';
 
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux";
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { router } from "./routers/router";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <RouterProvider router={router} />
+          <ToastContainer position="top-right" autoClose={3000} />
         </PersistGate>
       </Provider>
     </>
